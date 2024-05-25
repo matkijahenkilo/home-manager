00ebe8a8097884feadfca2598a66776f07dbd7fd
matkijahenkilo <matkija.henkilo@gmail.com>
2024-05-25 02:55:56 -0300

    Neovim config, etc

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,11 +22,13 @@
 
           # Specify your home configuration modules here, for example,
           # the path to your home.nix.
-          modules = [ ./home.nix ];
+          modules = [
+            ./home.nix 
+          ];
 
           # Optionally use extraSpecialArgs
           # to pass through arguments to home.nix
-	};
+	      };
       };
     };
 }
--- home.nix
+++ home.nix
@@ -1,95 +1,148 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
   home.username = "marisa";
   home.homeDirectory = "/home/marisa";
+  home.packages = [ ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-
-    ".config/fish/config.fish".text = ''
-      if status is-interactive
-        alias asd='yt-dlp -f mp4'
-      end
-    '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/marisa/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "nvim";
   };
 
   programs = {
+
+    # Important
     home-manager.enable = true;
+
     git = {
       enable = true;
       userName = "matkijahenkilo";
-      userEmail = "86783808+matkijahenkilo@users.noreply.github.com";
+      userEmail = "matkija.henkilo@gmail.com";
     };
-    fastfetch = {
+
+    neovim =
+    let
+      toLua = str: "lua << EOF\n${str}\nEOF\n";
+      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    in {
       enable = true;
-      settings.display = {
- #       binaryPrefix = "si";
-        color = "red";
-        separator = "  ";
+
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+
+      extraPackages = with pkgs; [
+        clang
+        fd
+        ripgrep
+
+        # Clipboard
+        wl-clipboard
+        xclip
+
+        # Commonly used LSPs
+        clang-tools
+        jdt-language-server
+        lua-language-server
+        nixd
+        pyright
+        texlab
+      ];
+
+      extraLuaPackages = lps: with lps; [
+        luautf8
+        jsregexp
+      ];
+
+      plugins = with pkgs.vimPlugins; [
+
+        {
+          plugin = gruvbox-nvim;
+          config = "colorscheme gruvbox";
+        }
+
+        neodev-nvim
+
+        cmp-buffer
+        cmp-cmdline
+        cmp-conjure
+        cmp_luasnip
+        cmp-nvim-lsp
+        cmp-path
+        cmp-rg
+        cmp-treesitter
+        nvim-cmp 
+        {
+          plugin = nvim-cmp;
+          config = toLuaFile ./modules/neovim/plugins/cmp.lua;
+        }
+
+        {
+          plugin = telescope-nvim;
+          config = toLuaFile ./modules/neovim/plugins/telescope.lua;
+        }
+        telescope-fzy-native-nvim
+
+        {
+          plugin = nvim-lspconfig;
+          config = toLuaFile ./modules/neovim/plugins/lsp.lua;
+        }
+
+        {
+          plugin = comment-nvim;
+          config = toLua "require(\"Comment\").setup()";
+        }
+
+        luasnip
+        friendly-snippets
+
+        lualine-nvim
+        lualine-lsp-progress
+        nvim-web-devicons
+
+        plenary-nvim
+        markdown-preview-nvim
+        Coqtail
+        conjure
+        emmet-vim
+        symbols-outline-nvim
+        nvim-web-devicons
+        vimspector
+        undotree
+        nerdcommenter
+        tagbar
+        vim-fugitive
+      ];
+
+      extraLuaConfig = ''
+        ${builtins.readFile ./modules/neovim/options.lua}
+      '';
+    };
+
+    # kitty = {
+    #   enable = true;
+    #   font = {
+    #     package = pkgs.nerdfonts;
+    #     name = "Fira Code nerd Font Mono";
+    #     size = 11;
+    #   };
+    #   settings = {
+    #     background_opacity = "0.85";
+    #     background_blur = "3";
+    #   };
+    # };
+
+    fish = {
+      enable = true;
+      functions = {
+        asd = {
+          body = "yt-dlp -f mp4 $argv[1]";
+          onEvent = "Ran yt-dlp";
+        };
       };
     };
+
+    fastfetch.enable = true;
   };
 }
